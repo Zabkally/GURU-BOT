0ee38c493a25038f93905a84c72959c76fe87c1d
Zabkally <145806833+Zabkally@users.noreply.github.com>
2023-09-23 15:17:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['254727585745', 'kemall', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
